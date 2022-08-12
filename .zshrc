7754ff677f0df4b59f6988f650493b4769c7cd4c
Andrew Chang <agc@mango1.local>
2022-08-12 12:37:18 -0700

    Update emacs and zsh
    - Add `init.el`
    - Add `.zshrc`

---
+++ .zshrc
@@ -0,0 +1,25 @@
+autoload -U promptinit && promptinit
+prompt elite2 yellow 
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/homebrew/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/homebrew/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/opt/homebrew/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/homebrew/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+export LS_COLORS="$LS_COLORS:di=0;36:ln=0;93:ex=0;35:"
+alias ls='gls -alhF --group-directories-first --color=auto'
+alias e='emacs'
---
+++ init.el
@@ -0,0 +1,30 @@
+(load-theme 'wombat)
+;; open in fullscreen
+(set-frame-parameter nil 'fullscreen 'fullboth)
+(setq mac-command-modifier 'meta)
+(setq inhibit-startup-screen t)
+(when (version<= "26.0.50" emacs-version )
+  (global-display-line-numbers-mode))
+
+;; melpa
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(magit ##)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Bindings (from Steve Yegge)
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
